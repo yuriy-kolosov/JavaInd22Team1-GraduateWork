4b0093a2ada0999a56d7e5ea35b0e9ab48ec208d
Distrog <Distrog@gmail.com>
2024-10-27 21:04:20 +0700

    добавил юзер сервис

--- src/main/java/ru/skypro/homework/controller/UserController.java
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -5,20 +5,27 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.NewPassword;
 import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.dto.User;
 
 import jakarta.validation.Valid;
+import ru.skypro.homework.service.UserService;
 
 @Tag(name = "Пользователи")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("users")
 public class UserController {
+
+
+    private final UserService userService;
 
     @Operation(summary = "Обновление пароля", tags = "Пользователи",
             responses = {
@@ -39,7 +46,8 @@ public class UserController {
                     )
             })
     @PostMapping("set_password")
-    public ResponseEntity<Void> setNewPassword(@Valid NewPassword newPassword) {
+    public ResponseEntity<Void> setNewPassword(@Valid @RequestBody NewPassword newPassword, Authentication authentication) {
+        userService.setNewPassword(newPassword, authentication);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
@@ -58,8 +66,9 @@ public class UserController {
                     )
             })
     @GetMapping("me")
-    public ResponseEntity<User> getInfo() {
-        return ResponseEntity.status(HttpStatus.OK).body(new User());
+    public ResponseEntity<User> getInfo(Authentication authentication) {
+        User user= userService.getInfo(authentication);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
     @Operation(summary = "Обновление информации об авторизованном пользователе", tags = "Пользователи",
@@ -77,8 +86,9 @@ public class UserController {
                     )
             })
     @PatchMapping("me")
-    public ResponseEntity<UpdateUser> changeUser(@Valid UpdateUser updateUser) {
-        return ResponseEntity.status(HttpStatus.OK).body(new UpdateUser());
+    public ResponseEntity<UpdateUser> changeUser(@Valid @RequestBody UpdateUser updateUser, Authentication authentication) {
+        UpdateUser user = userService.changeUser(updateUser, authentication);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
     @Operation(summary = "Обновление аватара авторизованного пользователя", tags = "Пользователи",
@@ -96,7 +106,8 @@ public class UserController {
                     )
             })
     @PatchMapping("me/image")
-    public ResponseEntity<Void> setImage(String image) {
+    public ResponseEntity<Void> setImage(String image, Authentication authentication) {
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+
 }
