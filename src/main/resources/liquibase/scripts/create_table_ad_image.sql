cd46c6c0089896480ee0f1b419cd1a32391aa48f
Mariia M <mariia_merkel@mail.ru>
2024-11-02 19:01:17 +0300

    hot fix

--- src/main/resources/liquibase/scripts/create_table_ad_image.sql
+++ src/main/resources/liquibase/scripts/create_table_ad_image.sql
@@ -2,7 +2,8 @@
 
 -- changeset yuriy-kolosov:11
 CREATE TABLE IF NOT EXISTS ad_image (
-  id bigserial NOT NULL REFERENCES ads(id) ON DELETE CASCADE,
+  id bigserial PRIMARY KEY,
+  ads_id INT8 NOT NULL REFERENCES ads (id) ON DELETE CASCADE,
   file_length INT8 NOT NULL,
   media_type VARCHAR(255) NOT NULL,
   data BYTEA
